ad0eb86340007cab021e91888c6e69fbadff8aec
RanchBean <ethr5627@740s-pl-111.local>
2015-10-07 13:49:33 -0600

    added ListViewer to controller

--- src/array/controller/ArrayListController.java
+++ src/array/controller/ArrayListController.java
@@ -1,19 +1,31 @@
 package array.controller;
 
 import java.util.ArrayList;
+import array.view.ListViewer;
 
 public class ArrayListController
 {
 
 	private ArrayList<String> firstWords;
+	private ListViewer showList;
+	
 	
 	public ArrayListController()
 	{
 		firstWords = new ArrayList<String>();
+		showList = new ListViewer();
 	}
 	public void start()
 	{
 		firstWords.add("aksdjlfh");
+		displayList();
 	}
 	
+	private void displayList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			showList.showList("The contents at " + spot + " are " + firstWords.get(spot));
+		}
+	}
 }
